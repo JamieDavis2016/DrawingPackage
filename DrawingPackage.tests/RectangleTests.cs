6c5cdacc2c37321648e69038c50299f3c0ea6696
Jamie <jamiehdavis96@gmail.com>
2024-08-15 10:30:46 +0100

    Small update

--- DrawingPackage.tests/RectangleTests.cs
+++ DrawingPackage.tests/RectangleTests.cs
@@ -50,5 +50,123 @@ namespace DrawingPackage.tests
             //assert
             sut.Should().Be("Rectangle (2,3) width=30 height=40");
         }
+
+        [Fact]
+        public void Update_width_of_rectangle()
+        {
+            //arrange
+            var x = 2;
+            var y = 3;
+            var width = 30;
+            var updatedWidth = 50;
+            var height = 40;
+            var sut = new Rectangle(x, y, width, height);
+
+            //act
+            sut.UpdateWidth(updatedWidth);
+
+
+            //assert
+            sut.Width.Should().Be(updatedWidth);
+        }
+
+        [Fact]
+        public void Update_height_of_rectangle()
+        {
+            //arrange
+            var x = 2;
+            var y = 3;
+            var width = 30;
+            var updatedHeight = 50;
+            var height = 40;
+            var sut = new Rectangle(x, y, width, height);
+
+            //act
+            sut.UpdateHeight(updatedHeight);
+
+            //assert
+            sut.Height.Should().Be(updatedHeight);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Update_width_of_rectangle_should_be_positive(int updatedWidth)
+        {
+            //arrange
+            var x = 2;
+            var y = 3;
+            var width = 50;
+            var height = 40;
+            var sut = new Rectangle(x, y, width, height);
+
+            //act
+            //assert
+            Invoking(() => sut.UpdateWidth(updatedWidth))
+                .Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Update_height_of_rectangle_should_be_positive(int updatedHeight)
+        {
+            //arrange
+            var x = 2;
+            var y = 3;
+            var width = 50;
+            var height = 40;
+            var sut = new Rectangle(x, y, width, height);
+
+            //act
+            //assert
+            Invoking(() => sut.UpdateHeight(updatedHeight))
+                .Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Undo_last_update_of_rectangle()
+        {
+            //arrange
+            var x = 2;
+            var y = 3;
+            var width = 30;
+            var updatedWidth = 50;
+            var height = 40;
+            var sut = new Rectangle(x, y, width, height);
+
+            sut.UpdateWidth(updatedWidth);
+            sut.Width.Should().Be(updatedWidth);
+
+            //act
+            sut.Undo();
+
+            //assert
+            sut.Width.Should().Be(width);
+            sut.HistoryUpdates.Count.Should().Be(0);
+        }
+
+        //[Fact]
+        //public void Undo_last_update_with_height_and_width_of_rectangle()
+        //{
+        //    //arrange
+        //    var x = 2;
+        //    var y = 3;
+        //    var width = 30;
+        //    var updatedWidth = 50;
+        //    var height = 40;
+        //    var sut = new Rectangle(x, y, width, height);
+
+        //    sut.UpdateWidth(updatedWidth);
+        //    sut.UpdateHeight(updatedWidth);
+        //    sut.Height.Should().Be(updatedWidth);
+
+        //    //act
+        //    sut.Undo();
+
+        //    //assert
+        //    sut.Height.Should().Be(width);
+        //    sut.HistoryUpdates.Count.Should().Be(1);
+        //}
     }
 }
--- DrawingPackage/Widgets/Rectangle.cs
+++ DrawingPackage/Widgets/Rectangle.cs
@@ -8,14 +8,38 @@ namespace DrawingPackage.Widgets
         {
             Width = EnsureArg.IsGt(width, 0);
             Height = EnsureArg.IsGt(height, 0);
+            HistoryUpdates = new Stack<Action>();
         }
 
         public int Width { get; private set; }
         public int Height { get; private set; }
+        public List<Action> HistoryUpdates { get; private set; }
 
         public override string Print()
         {
             return $"Rectangle ({X},{Y}) width={Width} height={Height}";
+        }
+
+        public void UpdateWidth(int width)
+        {
+            var original = Width;
+            HistoryUpdates.Push(() => this.Width = original);
+            Width = EnsureArg.IsGt(width, 0);
+        }
+
+        public void UpdateHeight(int height)
+        {
+            Height = EnsureArg.IsGt(height, 0);
+
+            HistoryUpdates.Push(() => this.Height = height);
+        }
+
+        public void Undo()
+        {
+            if(HistoryUpdates.Count > 0)
+            {
+                HistoryUpdates.Pop().Invoke();
+            }
         }
     }
 }
